4a1d75117d084ed61f9dba17e52abf6b4e33843f
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-04-18 18:15:28 +0500

    Update and rename BMI_Calculator_Assignment.ts to Table_Maker.ts

---
+++ Table_Maker.ts
@@ -0,0 +1,6 @@
+// let num = Number(prompt('Enter number'))  // to get number from user
+let num:number = 7
+// logic to create table of 7
+for (let i:number=1;i<11;i++){
+    console.log(`${num} x ${i} = ${i*num}`)
+}
