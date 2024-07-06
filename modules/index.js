515db94e2f656a55c6bc44af6a8b9867e72ca092
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-07-06 10:16:37 +0500

    Add files via upload

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// TYPESCRIPT MODULES
+var module_js_1 = require("./module.js");
+(0, module_js_1.default)();
+(0, module_js_1.MyFun1)();
+(0, module_js_1.MyFun2)();
---
+++ modules/module.ts
@@ -0,0 +1,11 @@
+export default function MyFun(){
+  console.log('THIS IS A DUMMY CODE')
+}
+function MyFun1(){
+  console.log('THIS IS A DUMMY CODE.')
+}
+function MyFun2(){
+  console.log('THIS IS A DUMMY CODE.')
+}
+export {MyFun1}
+export {MyFun2}
