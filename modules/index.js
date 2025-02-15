f48d412f79b360b0439503f2e5b970b17a568102
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2025-02-15 14:07:28 +0500

    Delete modules directory

--- modules/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// TYPESCRIPT MODULES
-var module_js_1 = require("./module.js");
-(0, module_js_1.default)();
-(0, module_js_1.MyFun1)();
-(0, module_js_1.MyFun2)();
--- modules/index.ts
+++
@@ -1,6 +0,0 @@
-// TYPESCRIPT MODULES
-import MyFun, { MyFun2, MyFun1 } from "./module.js"
-MyFun()
-MyFun1()
-MyFun2()
-
